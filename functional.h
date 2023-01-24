6a1986bce829b0d3660d2312aa3c0fbcc9ae4890
mn438628 <maciek.nowotek@gmail.com>
2023-01-24 11:32:03 +0100

    files

---
+++ functional.h
@@ -0,0 +1,8 @@
+//
+// Created by mn438628 on 24.01.23.
+//
+
+#ifndef IMAGES_FUNCTIONAL_H
+#define IMAGES_FUNCTIONAL_H
+
+#endif //IMAGES_FUNCTIONAL_H
---
+++ images.h
@@ -0,0 +1,8 @@
+//
+// Created by mn438628 on 24.01.23.
+//
+
+#ifndef IMAGES_IMAGES_H
+#define IMAGES_IMAGES_H
+
+#endif //IMAGES_IMAGES_H
